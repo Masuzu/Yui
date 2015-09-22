71348bb4c3933378caec4f838c6a5864b9eb6c82
Masuzu <anveena.menethil@gmail.com>
2015-09-22 14:21:46 +0200

    Update Skyline.cpp

--- Yui/Skyline.cpp
+++ Yui/Skyline.cpp
@@ -60,7 +60,7 @@ std::vector<std::pair<int, int>> GetSkyline(std::vector<std::vector<double>> &bu
 		}
 		else
 		{
-			// Erase from the height priority queue the starting edge of the building
+			// Erase from the height priority queue the starting edge of the building and add a keypoint if the current edge height is greater than the remaining edges in the priority queue 
 			double height = edge.height;
 			auto handle_it = handles.find(height);
 			heap.erase(handle_it->second);
